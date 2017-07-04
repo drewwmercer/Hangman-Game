46bb42b8b38ed88821fde89db459565bae33ce20
Andrew Mercer <japanexpress@gmail.com>
2017-07-03 21:01:43 -0400

    laid out main index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
 var terms = ["bootcampspot","javascript","container","github","sublime","console","devtools","bootstrap","react","node","database","mongodb","resume","linkedin","developer","application","certification","chrome","mozilla","heroku","gitlab"];
 
-ver termPlayed = terms[Math.floor(Math.random() * terms.length)];
+ver termToGuess = terms[Math.floor(Math.random() * terms.length)];
 
