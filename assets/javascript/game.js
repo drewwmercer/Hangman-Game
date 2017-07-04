d99b8d84b69d4c26b1979799c67b31a5ab0786c5
Andrew Mercer <japanexpress@gmail.com>
2017-07-03 21:58:53 -0400

    corrected typo in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var terms = ["bootcampspot","javascript","container","github","sublime","console","devtools","bootstrap","react","node","database","mongodb","resume","linkedin","developer","application","certification","chrome","mozilla","heroku","gitlab"];
 
-ver termToGuess = terms[Math.floor(Math.random() * terms.length)];
+var termToGuess = terms[Math.floor(Math.random() * terms.length)];
 
 var guessesLeft = 9;
 
