b23b932024b7ced4286a51a6de73dbc98bdce239
Andrew Mercer <japanexpress@gmail.com>
2017-07-03 20:51:23 -0400

    made updates to js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+var terms = ["bootcampspot","javascript","container","github","sublime","console","devtools","bootstrap","react","node","database","mongodb","resume","linkedin","developer","application","certification","chrome","mozilla","heroku","gitlab"];
+
+ver termPlayed = terms[Math.floor(Math.random() * terms.length)];
+
