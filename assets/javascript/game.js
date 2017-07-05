1227b98b61ee5ba5da7a776ec8166815142ea92e
Andrew Mercer <japanexpress@gmail.com>
2017-07-05 16:58:43 -0400

    updated game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,42 @@
-var terms = ["bootcampspot","javascript","container","github","sublime","console","devtools","bootstrap","react","node","database","mongodb","resume","linkedin","developer","application","certification","chrome","mozilla","heroku","gitlab"];
+var terms = ["bootcampspot",
+"javascript",
+"container",
+"github",
+"sublime",
+"console",
+"devtools",
+"bootstrap",
+"react",
+"node",
+"database",
+"mongodb",
+"resume",
+"linkedin",
+"developer",
+"application",
+"certification",
+"chrome",
+"mozilla",
+"heroku",
+"gitlab"];
 
 var termToGuess = terms[Math.floor(Math.random() * terms.length)];
 
-var guessesLeft = 9;
+var guessesLeft = 8;
 
 console.log(guessesLeft);
 
-document.getElementById("guesses-left").innerHTML = guessesLeft;
+guesses = document.getElementById("guesses-left").innerHTML;
 
 console.log(guessesLeft);
 
 
 
-
-// 1. freebie
-// 2. post
-// 3. head
-// 4. body
+// 1. post
+// 2. head
+// 3. body
+// 4. arm
 // 5. arm
-// 6. arm
+// 6. leg 
 // 7. leg 
-// 8. leg 
-// 9. noose
+// 8. noose
