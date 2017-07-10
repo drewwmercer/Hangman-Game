1d2da81658b2d283f4f792ee0f5265314658e88f
Andrew Mercer <japanexpress@gmail.com>
2017-07-10 19:19:04 -0400

    updated references and formatted docs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,188 +1,132 @@
-// valid letters that can be guessed
-var validLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// array of terms that can populate the blanks
+var winCounter = 0;
+var placeholderArray = [];
+var prevPlaceholderArray = [];
+var blankSpaces = [];
+var lettersGuessed = [];
+var word = [];
+var wordPlaceholderString = "";
+var letterGuessed = "";
+var correctGuessCount = 0;
+var guessesLeft = 8;
 var terms = ["bootcampspot", "javascript", "container", "github", "sublime", "console", "devtools", "bootstrap", "react", "node", "database", "mongodb", "resume", "linkedin", "developer", "application", "certification", "chrome", "mozilla", "heroku", "gitlab", "jquery"];
 
-// defining variables
-var wrongGuessesLeft = 8;  // default remaining wrong guesses
-var correctLettersLeft;
-var letterGuessed;
-var lettersAlreadyGuessed = [];  // empty array to show the letters pressed already
-var winCounter = 0;  // count the number of wins and put it on the page
-var termToGuess;
-var blankSpaces = [];
+createWord(terms);
 
-// ** testing
+document.onkeyup = function (event) {
+    console.log("key pressed: ", event.key);
+    var letterGuessed = event.key;
+    console.log("letter guessed: " + letterGuessed);
+    document.getElementById("instruction").innerHTML = "<strong>" + "Good luck!" + "</strong>";
+    var keyPress;
+    if (typeof event != 'undefined') {
+        keyPress = event.keyCode;
 
-// var termToGuess = terms[Math.floor(Math.random() * terms.length)];
-// var blankSpaces = [];
-// for (var i = 0; i < termToGuess.length; i++) {
-//         blankSpaces[i] = "_";
-//     }
-// var correctLettersLeft = termToGuess.length;
+        letterGuessed = String.fromCharCode(keyPress).toLowerCase();
+        console.log(letterGuessed + " should match the key entered");
 
-// document.getElementById("win-counter").innerHTML = "Wins: " + "<strong>" + winCounter + "</strong>";
-// document.getElementById("blank-spaces").innerHTML = blankSpaces.join(" ");
-// document.getElementById("guesses-left").innerHTML = "Number of incorrect guesses remaining: " + "<strong>" + wrongGuessesLeft + "</strong>";
+        trackLetterGuesses(letterGuessed);
 
-// while (correctLettersLeft > 0) {
-//     document.getElementById("blank-spaces").innerHTML = blankSpaces.join(" ");
-// }
+        buildWord(letterGuessed);
+    }
+    else if (e) {
+        keyPress = e.which;
+    }
+    return false;
+};
 
-// ** testing
-
-function gameLoad() {
+function createWord(terms) {
     termToGuess = terms[Math.floor(Math.random() * terms.length)];
+    console.log(termToGuess);
     blankSpaces = [];
     for (var i = 0; i < termToGuess.length; i++) {
         blankSpaces[i] = "_";
     }
     document.getElementById("blank-spaces").innerHTML = blankSpaces.join(" ");
 
-document.onkeyup = function () {
-            document.getElementById("instruction").innerHTML = "<strong>" + "Good luck!" + "</strong>";
-            var letterGuessed = event.key;
-            console.log("letter guessed: " + letterGuessed);
-            lettersAlreadyGuessed.push(letterGuessed);
-            if (letterGuessed.indexOf(validLetters) > -1) {
-                for (var j = 0; j < termToGuess.length; j++) {
-                    if (letterGuessed === termToGuess[i]) {
-                        blankSpaces[j] = letterGuessed;
-                        correctLettersLeft--;
-                    }
-                    if (letterGuessed !== termToGuess[j]) {
-                        wrongGuessesLeft--;
-                    }
-                }
+    createWordPlaceholder(termToGuess);
+    return termToGuess;
+};
 
-            }
-            document.getElementById("incorrect-letters").innerHTML = "Letters you have guessed: " + "<strong>" + lettersAlreadyGuessed + "</strong>";
-
-        }
-
-}
-gameLoad();
-
-
-function gamePlay() {
-      document.onkeyup = function () {
-            document.getElementById("instruction").innerHTML = "<strong>" + "Good luck!" + "</strong>";
-            var letterGuessed = event.key;
-            console.log("letter guessed: " + letterGuessed);
-            lettersAlreadyGuessed.push(letterGuessed);
-            if (letterGuessed.indexOf(validLetters) > -1) {
-                for (var j = 0; j < termToGuess.length; j++) {
-                    if (letterGuessed === termToGuess[i]) {
-                        blankSpaces[j] = letterGuessed;
-                        correctLettersLeft--;
-                    }
-                    if (letterGuessed !== termToGuess[j]) {
-                        wrongGuessesLeft--;
-                    }
-                }
-
-            }
-            document.getElementById("incorrect-letters").innerHTML = "Letters you have guessed: " + "<strong>" + lettersAlreadyGuessed + "</strong>";
-
-        }
-        // }
-        //lettersAlreadyGuessed = lettersAlreadyGuessed.push(letterGuessed);
-    }
-//gamePlay();
-
-
-
-
-function gameLayout() {
-
-    // randomly selecting a word from the terms array
-    var termToGuess = terms[Math.floor(Math.random() * terms.length)];
-    console.log("term: " + termToGuess);
-    // correct guesses left to be guessed
-    var correctLettersLeft = termToGuess.length;
-    console.log("letters left: " + correctLettersLeft);
-    // make an empty array for blank spaces
+function createWordPlaceholder(termToGuess) {
     var blankSpaces = [];
-    // set up the blank spaces for the selected word
     for (var i = 0; i < termToGuess.length; i++) {
         blankSpaces[i] = "_";
     }
-    console.log("blank spaces: " + blankSpaces.join(" "));
-
-    document.getElementById("win-counter").innerHTML = "Wins: " + "<strong>" + winCounter + "</strong>";
     document.getElementById("blank-spaces").innerHTML = blankSpaces.join(" ");
-    document.getElementById("guesses-left").innerHTML = "Number of incorrect guesses remaining: " + "<strong>" + wrongGuessesLeft + "</strong>";
 
-    var blankSpaces = correctLettersLeft;
+    return blankSpaces;
+};
 
-    function playerMove() {
-        document.onkeyup = function () {
-            document.getElementById("instruction").innerHTML = "<strong>" + "Good luck!" + "</strong>";
-            var letterGuessed = event.key;
-            console.log("letter guessed: " + letterGuessed);
-            lettersAlreadyGuessed.push(letterGuessed);
-            if (letterGuessed.indexOf(validLetters) > -1) {
-                for (var j = 0; j < termToGuess.length; j++) {
-                    if (letterGuessed === termToGuess[i]) {
-                        blankSpaces[j] = letterGuessed;
-                        correctLettersLeft--;
-                    }
-                    if (letterGuessed !== termToGuess[j]) {
-                        wrongGuessesLeft--;
-                    }
-                }
-
-            }
-            document.getElementById("incorrect-letters").innerHTML = "Letters you have guessed: " + "<strong>" + lettersAlreadyGuessed + "</strong>";
-
-        }
-        // }
-        //lettersAlreadyGuessed = lettersAlreadyGuessed.push(letterGuessed);
-    }
-    // playerMove();
-
-    function checkGuess() {
-        for (var j = 0; j < termToGuess.length; j++) {
-            if (letterGuessed === termToGuess[j]) {
-                blankSpaces[j] = letterGuessed;
-                correctLettersLeft--;
-            }
-            if (letterGuessed !== termToGuess[j]) {
-                wrongGuessesLeft--;
-            }
-            if (correctLettersLeft === 0) {
-                winCounter++;
-            }
-
-            document.getElementById("incorrect-letters").innerHTML = "Letters you have guessed: " + "<strong>" + lettersAlreadyGuessed + "</strong>";
+function trackLetterGuesses(letterGuessed) {
+    for (i = 0; i < lettersGuessed.length; i++) {
+        if (letterGuessed == lettersGuessed[i]) {
+            return;
         }
     }
-   // checkGuess();
-}
-//gameLayout();  // calling the function above
 
-// need loss
-// need win
-// if (correctLettersLeft > 0 && wrongGuessesLeft > 0) {  // word hasn't been completed and not out of guesses
+    lettersGuessed.push(letterGuessed);
+    console.log("LettersGuessed array item: " + lettersGuessed[0]);
 
-// function gamePlay(event) {
+    var lettersGuessedString = lettersGuessed.join(", ");
+    document.getElementById("incorrect-letters").innerHTML = "Letters you have guessed: " + "<strong>" + lettersGuessedString + "</strong>";
+    guessesLeft--;
 
-// if (correctLettersLeft > 0 && wrongGuessesLeft > 0) {
-// var testOne = document.getElementById("blank-spaces");
-// testOne.innerHTML = blankSpaces.join(" ") // this puts blank spaces between the underscores
+    document.getElementById("guesses-left").innerHTML = "Number of incorrect guesses remaining: " + "<strong>" + guessesLeft + "</strong>";
+    console.log('Guesses left: ' + guessesLeft);
 
-//}
-// console.log(letterGuessed); // take me out later
-// for (var j = 0; j < termToGuess.length; j++) {
-//     if (termToGuess[j] === letterGuessed) {
-//         blankSpaces[j] = letterGuessed;
-//         correctLettersLeft--;
-//     }
-// }
+    if (guessesLeft == 0) {
+        resetBoard();
+    }
 
-// document.getElementById("guesses-left").innerHTML = wrongGuessesLeft;
-        // }
-// gamePlay(event);
+    return lettersGuessedString;
+};
 
-// How to hang: 1. post, 2. head, 3. body, 4. arm, 5. arm, 6. leg, 7. leg, 8. noose
+function buildWord(letterGuessed) {
+
+    if (prevPlaceholderArray.length == 0) {
+        placeholderArray = createWordPlaceholder(termToGuess);
+
+    } else {
+        placeholderArray = prevPlaceholderArray;
+    }
+
+    for (var i = 0; i < termToGuess.length; i++) {
+        console.log('Word is ' + termToGuess);
+        if (letterGuessed == termToGuess[i]) {
+            console.log(letterGuessed + " is in the word at index " + i);
+            placeholderArray[i] = letterGuessed;
+            guessesLeft++;
+        }
+    }
+
+    prevPlaceholderArray = placeholderArray;
+
+    placeholder = placeholderArray.join(" ");
+    document.getElementById('blank-spaces').innerHTML = placeholder;
+
+    console.log("Placeholder Array length is " + placeholderArray.length);
+    console.log("Placeholder split is " + placeholder.split(","));
+
+    var termNeedArray = Array.prototype.slice.call(termToGuess);
+
+    if (placeholder === termNeedArray.join(" ")) {
+        console.log("Woot");
+        winCounter++;
+        document.getElementById("win-counter").innerHTML = "Wins: " + "<strong>" + winCounter + "</strong>";
+        resetBoard();
+    }
+};
+
+function resetBoard(blankSpaces) {
+    createWord(terms);
+
+    letterGuessed = "";
+    prevPlaceholderArray = [];
+    placeholderArray = [];
+    guessesLeft = 8;
+    correctGuessCount = 0;
+    lettersGuessed = [];
+
+    document.getElementById('guesses-left').innerHTML = guessesLeft;
+    document.getElementById('incorrect-letters').innerHTML = lettersGuessed;
+};
